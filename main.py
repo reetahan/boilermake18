8562b3bf063c4815c6ea52aefc95d4de513db8c1
Reetahan <rm10@illinois.edu>
2018-10-20 21:53:27 -0400

    ok

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+import numpy as np
+
+
+
+
+def main():
+    var = input("Welcome to the Neighborhood Suggestion Machine!: ")
+    print ("you entered", var)
+
+
+if __name__ == '__main__':
+    main()
